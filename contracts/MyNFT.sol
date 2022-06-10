80ed8cc36d53f8c1a39d57df526c7d38bd175be4
unnc-fse-JiaweiWu <scyjw2@nottingham.edu.cn>
2022-06-10 19:00:15 +0800

    add status

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -15,6 +15,11 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 contract MyNFT is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+    bool internal _transferable;
+    bool internal _mintable;
+    bool internal _burnable
+
+
 
     constructor() ERC721("MyNFT", "NFT") {}
     //address recipient specifies the address that will receive your freshly minted NFT
@@ -61,5 +66,44 @@ contract MyNFT is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Ow
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
+    }
+
+
+     // Added isTransferable only
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public override isTransferable {
+        //solhint-disable-next-line max-line-length
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "ERC721: transfer caller is not owner nor approved"
+        );
+
+        _transfer(from, to, tokenId);
+    }
+
+    // Added isTransferable only
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory _data
+    ) public override isTransferable {
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "ERC721: transfer caller is not owner nor approved"
+        );
+        _safeTransfer(from, to, tokenId, _data);
+    }
+
+    modifier isTransferable() {
+        require(transferable() == true, "Memberships: not transferable");
+        _;
+    }
+
+    function transferable() public view returns (bool) {
+        return _transferable;
     }
 }
