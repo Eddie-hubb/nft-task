0309e1eab2edd500410d12ad0d8f3a3c014c32ea
unnc-fse-JiaweiWu <scyjw2@nottingham.edu.cn>
2022-06-11 16:48:50 +0800

    add class for nft metadata

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -8,24 +8,77 @@
     import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
     import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
+    contract NFTProperty is Ownable {
+        address private NFTOwner;
+        uint256 private class_id;
+        bool private transferable = true;
+        bool private mintable = true;
+        bool private burnable = true;
+        bool private frozen = false;
+
+
+        function setNFTOwner(address _nftOwner) public {
+            NFTOwner = _nftOwner;
+        }
+
+        // function transferNFTOwnership(address _newNFTOwner) public {
+
+        // }
+
+        function getTransferable() public view returns (bool) {
+            return transferable;
+        }
+
+        function flipTransferable() public onlyOwner {
+            require(frozen == false, "the nft is frozen");
+            transferable = !transferable;
+        }
+        
+
+        function getBurnable() public view returns (bool) {
+            return burnable;
+        }
+
+        function flipBurnable() public onlyOwner {
+            require(frozen == false, "the nft is frozen");
+            burnable = !burnable;
+        }
+
+        function getMintable() public view returns (bool) {
+            return mintable;
+        }
+
+        function flipMintable() public onlyOwner {
+            require(frozen == false, "the nft is frozen");
+            mintable = !mintable;
+        }   
+
+        function getFrozen() public view returns (bool) {
+            return frozen;
+        }
+
+        function flipFrozen() public onlyOwner{
+            frozen = !frozen;
+        }
+
+
+    }
+
 
 
 
 
     contract MyNFT is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Ownable {
         using Counters for Counters.Counter;
-
+        NFTProperty private nftProperty = new NFTProperty();
+        uint256 private property_id;
         Counters.Counter private _tokenIds;
-
-        bool private _transferable = true;
-        bool private _mintable = true;
-        bool private _burnable = true;
-        bool private _frozen = true;
-
 
         //具有转账功能的智能合约的 constructor 必须显式的指定为 payable。
         //
-        constructor() ERC721("MyNFT", "NFT") payable {}
+        constructor() ERC721("MyNFT", "NFT") payable {
+            nftProperty.setNFTOwner(msg.sender);
+        }
         //address recipient specifies the address that will receive your freshly minted NFT
         //string memory tokenURI is a string that should resolve to a JSON document that describes the NFT's metadata.
         //mintNFT calls some methods from the inherited ERC-721 library, and ultimately returns a number that represents the ID of the freshly minted NFT
@@ -34,7 +87,7 @@
             public 
             returns (uint256)
         {
-            require(_mintable, "the NFT is not mintable");
+            require(nftProperty.getMintable() == true, "the NFT is not mintable");
             _tokenIds.increment();
             uint256 newItemId = _tokenIds.current();
             _safeMint(msg.sender, newItemId);
@@ -62,7 +115,7 @@
         }
 
         function burnNFT(uint256 tokenId) public {
-            require(_burnable == true, "The nft is not burnable");
+            require(nftProperty.getBurnable() == true, "The nft is not burnable");
             _burn(tokenId);
         }
 
@@ -96,7 +149,7 @@
                 _isApprovedOrOwner(_msgSender(), tokenId),
                 "ERC721: transfer caller is not owner nor approved"
             );
-            require(_transferable == true, "The nft is not burnable");
+            require(nftProperty.getTransferable() == true, "The nft is not burnable");
 
 
             _transfer(from, to, tokenId);
@@ -113,52 +166,62 @@
                 _isApprovedOrOwner(_msgSender(), tokenId),
                 "ERC721: transfer caller is not owner nor approved"
             );
-            require(_transferable == true, "The nft is not burnable");
+            require(
+                nftProperty.getTransferable() == true, 
+                "The nft is not burnable"
+                );
 
             _safeTransfer(from, to, tokenId, _data);
         }
 
         modifier isTransferable() {
-            require(_transferable == true, "Memberships: not transferable");
+            require(
+                nftProperty.getTransferable() == true, 
+                "Memberships: not transferable"
+            );
             _;
         }
 
+        // if the condition is satisfied, the function will run, otherwise it will throw exception (modifier)
+
+
         function getTransferable() public view returns (bool) {
-            return _transferable;
+            return nftProperty.getTransferable();
         }
 
-        function flipTransferable() public onlyOwner {
-            require(_frozen == false, "the nft is frozen");
-            _transferable = !_transferable;
+        function flipTransferable() public  {
+            require(nftProperty.getFrozen() == false, "the nft is frozen");
+            nftProperty.flipTransferable();
         }
         
-         // if the condition is satisfied, the function will run, otherwise it will throw exception (modifier)
 
         function getBurnable() public view returns (bool) {
-            return _burnable;
+            return nftProperty.getBurnable();
         }
 
-        function flipBurnable() public onlyOwner {
-            require(_frozen == false, "the nft is frozen");
-            _burnable = !_burnable;
+        function flipBurnable() public {
+            require(nftProperty.getFrozen() == false, "the nft is frozen");
+            nftProperty.flipBurnable();
         }
 
         function getMintable() public view returns (bool) {
-            return _mintable;
+            return nftProperty.getMintable();
         }
 
-        function flipMintable() public onlyOwner {
-            require(_frozen == false, "the nft is frozen");
-            _mintable = !_mintable;
+        function flipMintable() public {
+            require(nftProperty.getFrozen() == false, "the nft is frozen");
+            nftProperty.flipMintable();
         }   
 
         function getFrozen() public view returns (bool) {
-            return _frozen;
+            return nftProperty.getFrozen();
         }
 
-        function flipFrozen() public onlyOwner{
-            _frozen = !_frozen;
+        function flipFrozen() public{
+            nftProperty.flipFrozen();
         }
 
     }
 
+
+    
